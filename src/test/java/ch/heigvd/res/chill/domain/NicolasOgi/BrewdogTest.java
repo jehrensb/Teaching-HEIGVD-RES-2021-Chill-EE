a3c4acd76adacb242787fd87b1120af299a663be
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-02-26 14:28:28 +0100

    Correction of BrewdogTest.java

--- src/test/java/ch/heigvd/res/chill/domain/NicolasOgi/BrewdogTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/NicolasOgi/BrewdogTest.java
@@ -1,7 +1,6 @@
 package ch.heigvd.res.chill.domain.NicolasOgi;
 
 import ch.heigvd.res.chill.domain.Bartender;
-import ch.heigvd.res.chill.domain.wasadigi.PunkIPA;
 import ch.heigvd.res.chill.protocol.OrderRequest;
 import ch.heigvd.res.chill.protocol.OrderResponse;
 import org.junit.jupiter.api.Test;
@@ -13,16 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BrewdogTest {
 
   @Test
-  void thePriceAndNameForPunkIPAShouldBeCorrect() {
+  void thePriceAndNameForBrewdogShouldBeCorrect() {
     Brewdog beer = new Brewdog();
     assertEquals(beer.getName(), Brewdog.NAME);
     assertEquals(beer.getPrice(), Brewdog.PRICE);
   }
 
   @Test
-  void aBartenderShouldAcceptAnOrderForPunkIPA() {
+  void aBartenderShouldAcceptAnOrderForBrewdog() {
     Bartender jane = new Bartender();
-    String productName = "ch.heigvd.res.chill.domain.wasadigi.PunkIPA";
+    String productName = "ch.heigvd.res.chill.domain.NicolasOgi.Brewdog";
     OrderRequest request = new OrderRequest(3, productName);
     OrderResponse response = jane.order(request);
     BigDecimal expectedTotalPrice = Brewdog.PRICE.multiply(new BigDecimal(3));
