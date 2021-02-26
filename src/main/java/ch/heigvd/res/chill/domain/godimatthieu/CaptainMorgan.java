3ea8b568eaec78008718843d2f14c14981c693a3
CapitainMorgan <40363984+CapitainMorgan@users.noreply.github.com>
2021-02-26 15:23:23 +0100

    First version of CaptainMorgan drink

---
+++ src/main/java/ch/heigvd/res/chill/domain/godimatthieu/CaptainMorgan.java
@@ -0,0 +1,20 @@
+package ch.heigvd.res.chill.domain.godimatthieu;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class CaptainMorgan implements IProduct {
+    public final static String NAME = "Captain Morgan";
+    public final static BigDecimal PRICE = new BigDecimal(12.0);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
