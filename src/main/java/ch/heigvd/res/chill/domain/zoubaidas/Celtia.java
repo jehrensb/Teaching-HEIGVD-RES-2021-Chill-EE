90b86948195324f5ae434b38ff1fec9ee79f7935
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2021-02-27 15:13:49 +0100

    Add Celtia beer with tests

---
+++ src/main/java/ch/heigvd/res/chill/domain/zoubaidas/Celtia.java
@@ -0,0 +1,21 @@
+package ch.heigvd.res.chill.domain.zoubaidas;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Celtia implements IProduct {
+
+    public final static String NAME = "Celtia";
+    public final static BigDecimal PRICE = new BigDecimal(0.5);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
