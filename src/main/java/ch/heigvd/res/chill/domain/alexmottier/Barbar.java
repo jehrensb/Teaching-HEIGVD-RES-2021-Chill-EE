3885396476ed12b3d8593a9857e12f378e0368b3
alex-mottier <alexandre.mottier@heig-vd.ch>
2021-03-04 15:11:44 +0100

    add Barabr + tests

--- src/main/java/ch/heigvd/res/chill/domain/alexmottier/Barbar.java
+++ src/main/java/ch/heigvd/res/chill/domain/alexmottier/Barbar.java
@@ -1,2 +1,21 @@
-package ch.heigvd.res.chill.domain.alexmottier;public class Barbar {
+package ch.heigvd.res.chill.domain.alexmottier;
+
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Barbar implements IProduct {
+    public final static String NAME = "Barbar";
+    public final static BigDecimal PRICE = new BigDecimal(5.5);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
 }
